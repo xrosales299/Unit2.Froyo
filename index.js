cbd0902db626753774aac069b55c2526becbb07f
wewman222 <77263223+wewman222@users.noreply.github.com>
2025-03-26 20:03:04 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
+const arr = order.split(",")
+
+const stats = {}
+
+
+
 function orderup(){
     let order = prompt("Please order flavors seperated by commas", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
     if (order != null) {
-        document.getElementById("ordertext").innerHTML = "You have ordered " + order;
+        document.getElementById("ordertext").innerHTML = "You have ordered" + stats[flavor];
     }
 }
 
+for(let i = 0; i < arr.length; i++){
+    const flavor = arr[i]
+    console.log(flavor)
+    if(stats[flavor] === undefined){
+        stats[flavor] = 0
+    }
+    stats[flavor]++
+}
